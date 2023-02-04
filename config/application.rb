422edd5a8e56793bb05453a557915747963c70a8
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-02-05 02:38:09 +0300

    edit application

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,8 @@ require 'active_job/railtie'
 require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require 'action_view/railtie'
-# require "action_cable/engine"
-# require "rails/test_unit/railtie"
-
+require 'rack/throttle'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -26,15 +21,8 @@ module WeatherApi
     config.load_defaults 7.0
 
     # Минимальный интервал между последовательными HTTP-запросами 5 сек.
-    config.middleware.use Rack::Throttle::Interval, min: 5 if Rails.env.production?
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use Rack::Throttle::Interval, min: 5
+    # config.middleware.use Rack::Throttle::Interval, min: 5 if Rails.env.production?
 
     # Don't generate system test files.
     config.generators.system_tests = nil
