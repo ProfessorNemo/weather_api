370e6765740a9ed8a18edc1439c5084e428e2c7b
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-08-08 16:02:53 +0300

    Revert "norm-1"
    This reverts commit 9b0edd9e8750874a83b22f892385dc4952d2e7d1.

--- app/services/get_client.rb
+++ app/services/get_client.rb
@@ -3,4 +3,67 @@
 require 'database_cleaner/active_record'
 
 class GetClient
+  CITY = 'St.Petersburg'
+  BASE_URL = 'https://dataservice.accuweather.com'
+  CITY_URL = "#{BASE_URL}/locations/v1/cities/search?".freeze
+
+  def create_data
+    request.each do |data|
+      WeatherForecast.where(date: data['EpochTime'])
+                     .first_or_create(temperature: data.dig('Temperature', 'Metric', 'Value'))
+    end
+  end
+
+  # обновление прогноза
+  def update_data
+    DatabaseCleaner.strategy = [:truncation, { only: :weather_forecasts }]
+    DatabaseCleaner.clean
+
+    create_data
+  end
+
+  private
+
+  # запрос на получения ключа города
+  def location_key
+    connection = Faraday.new(
+      url: CITY_URL,
+      params: options
+    )
+    response ||= connection.get(CITY_URL,
+                                params: options,
+                                headers: { 'Content-type' => 'application/json' })
+  rescue Faraday::Error => e
+    Rails.logger.error { "Ошибка соединения с сервером: #{e.message}" }
+    abort e.message
+  else
+    begin
+      city_key = respond_with(response)
+      city_key[0]['Key']
+    rescue StandardError => e
+      Rails.logger.error { "Ошибка в названии города!: #{e.message}" }
+      abort e.message
+    end
+  end
+
+  # запрос на получения данных о погоде
+  def request
+    response ||= Faraday.get("#{BASE_URL}/currentconditions/v1/#{location_key}/historical/24?",
+                             options.slice(:apikey))
+
+    respond_with(response)
+  end
+
+  def respond_with(response)
+    raise 'Не удалось получить данные с сервера' if response.body == 'null'
+
+    JSON.parse(response.body)
+  end
+
+  def options
+    {
+      apikey: Rails.application.credentials[:token],
+      q: CITY
+    }
+  end
 end
