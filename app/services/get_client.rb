a48cd14d6ebc36f89e6a101c4197479bebb727c3
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-08-08 15:43:38 +0300

    3

--- app/services/get_client.rb
+++ app/services/get_client.rb
@@ -6,19 +6,4 @@ class GetClient
   CITY = 'St.Petersburg'
   BASE_URL = 'https://dataservice.accuweather.com'
   CITY_URL = "#{BASE_URL}/locations/v1/cities/search?".freeze
-
-  def create_data
-    request.each do |data|
-      WeatherForecast.where(date: data['EpochTime'])
-                     .first_or_create(temperature: data.dig('Temperature', 'Metric', 'Value'))
-    end
-  end
-
-  # обновление прогноза
-  def update_data
-    DatabaseCleaner.strategy = [:truncation, { only: :weather_forecasts }]
-    DatabaseCleaner.clean
-
-    create_data
-  end
 end
