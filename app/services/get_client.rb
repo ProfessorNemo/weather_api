815d62415b41ce295a945cf2805e9b8501c4b367
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-08-08 15:56:47 +0300

    22

--- app/services/get_client.rb
+++ app/services/get_client.rb
@@ -6,43 +6,4 @@ class GetClient
   CITY = 'St.Petersburg'
   BASE_URL = 'https://dataservice.accuweather.com'
   CITY_URL = "#{BASE_URL}/locations/v1/cities/search?".freeze
-
-  def create_data
-    request.each do |data|
-      WeatherForecast.where(date: data['EpochTime'])
-                     .first_or_create(temperature: data.dig('Temperature', 'Metric', 'Value'))
-    end
-  end
-
-  # обновление прогноза
-  def update_data
-    DatabaseCleaner.strategy = [:truncation, { only: :weather_forecasts }]
-    DatabaseCleaner.clean
-
-    create_data
-  end
-
-  private
-
-  # запрос на получения ключа города
-  def location_key
-    connection = Faraday.new(
-      url: CITY_URL,
-      params: options
-    )
-    response ||= connection.get(CITY_URL,
-                                params: options,
-                                headers: { 'Content-type' => 'application/json' })
-  rescue Faraday::Error => e
-    Rails.logger.error { "Ошибка соединения с сервером: #{e.message}" }
-    abort e.message
-  else
-    begin
-      city_key = respond_with(response)
-      city_key[0]['Key']
-    rescue StandardError => e
-      Rails.logger.error { "Ошибка в названии города!: #{e.message}" }
-      abort e.message
-    end
-  end
 end
