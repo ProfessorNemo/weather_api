a20465831331edb03c94d04764a9d8115bdb74fa
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-08-08 15:40:07 +0300

    Revert "norm-2"
    This reverts commit 69ed58bcc425c85e99f6665b9a0aa953ab6bb921.

--- app/services/get_client.rb
+++ app/services/get_client.rb
@@ -6,4 +6,11 @@ class GetClient
   CITY = 'St.Petersburg'
   BASE_URL = 'https://dataservice.accuweather.com'
   CITY_URL = "#{BASE_URL}/locations/v1/cities/search?".freeze
+
+  def create_data
+    request.each do |data|
+      WeatherForecast.where(date: data['EpochTime'])
+                     .first_or_create(temperature: data.dig('Temperature', 'Metric', 'Value'))
+    end
+  end
 end
