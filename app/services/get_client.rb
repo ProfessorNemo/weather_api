b5575b35da2d7642017890a1de25108db63d836e
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2023-08-08 15:42:35 +0300

    1

--- app/services/get_client.rb
+++ app/services/get_client.rb
@@ -21,4 +21,49 @@ class GetClient
 
     create_data
   end
+
+  private
+
+  # запрос на получения ключа города
+  def location_key
+    connection = Faraday.new(
+      url: CITY_URL,
+      params: options
+    )
+    response ||= connection.get(CITY_URL,
+                                params: options,
+                                headers: { 'Content-type' => 'application/json' })
+  rescue Faraday::Error => e
+    Rails.logger.error { "Ошибка соединения с сервером: #{e.message}" }
+    abort e.message
+  else
+    begin
+      city_key = respond_with(response)
+      city_key[0]['Key']
+    rescue StandardError => e
+      Rails.logger.error { "Ошибка в названии города!: #{e.message}" }
+      abort e.message
+    end
+  end
+
+  # запрос на получения данных о погоде
+  def request
+    response ||= Faraday.get("#{BASE_URL}/currentconditions/v1/#{location_key}/historical/24?",
+                             options.slice(:apikey))
+
+    respond_with(response)
+  end
+
+  def respond_with(response)
+    raise 'Не удалось получить данные с сервера' if response.body == 'null'
+
+    JSON.parse(response.body)
+  end
+
+  def options
+    {
+      apikey: Rails.application.credentials[:token],
+      q: CITY
+    }
+  end
 end
